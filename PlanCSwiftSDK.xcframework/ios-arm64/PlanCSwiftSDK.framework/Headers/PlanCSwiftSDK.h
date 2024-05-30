d687a63d41f2e1d78479cc7505a179bfe16d582b
侯俊岗 <33799939+HouJunGang@users.noreply.github.com>
2024-05-30 11:16:29 +0800

    Add files via upload

---
+++ PlanCSwiftSDK.xcframework/ios-arm64/PlanCSwiftSDK.framework/Headers/PlanCSwiftSDK.h
@@ -0,0 +1,18 @@
+//
+//  PlanCSwiftSDK.h
+//  PlanCSwiftSDK
+//
+//  Created by 侯俊岗 on 2024/1/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PlanCSwiftSDK.
+FOUNDATION_EXPORT double PlanCSwiftSDKVersionNumber;
+
+//! Project version string for PlanCSwiftSDK.
+FOUNDATION_EXPORT const unsigned char PlanCSwiftSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlanCSwiftSDK/PublicHeader.h>
+
+
